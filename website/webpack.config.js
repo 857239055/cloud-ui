b53d0e4744c31d6defaeeb05aaefa5fbee354126
857239055 <857239055@qq.com>
2021-08-01 11:51:15 +0800

    init

--- website/webpack.config.js
+++ website/webpack.config.js
@@ -1,6 +1,11 @@
+const path = require('path')
 
 module.exports = {
     mode: 'development',
     devtool: 'source-map',
-    
+    entry: path.resolve(__dirname, 'main.ts'),
+    output: {
+        path: path.resolve(__dirname, '../website-dist'),
+        filename: "bundle.js"
+    }
 }
