42e2302341008a06c3e0d95f167cba91385aa448
857239055 <857239055@qq.com>
2021-08-01 16:56:35 +0800

    init

--- website/App.vue
+++ website/App.vue
@@ -1,10 +1,16 @@
 <template>
-
+  <div>
+    <cl-button></cl-button>
+    <cl-icon></cl-icon>
+  </div>
 </template>
 
 <script>
+import ClButton from "@cloud-ui/button";
+import ClIcon from "@cloud-ui/icon";
 export default {
-  name: "App"
+  name: "App",
+  components: {ClIcon, ClButton}
 }
 </script>
 
--- website/webpack.config.js
+++ website/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {VueLoaderPlugin} = require('vue-loader')
 
 module.exports = {
     mode: 'development',
@@ -7,5 +9,39 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, '../website-dist'),
         filename: "bundle.js"
-    }
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.vue']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /.vue$/,
+                loader: "vue-loader"
+            },
+            {
+                test: /\.(svg|otf|ttf|woff|woff2|eot|gif|png)$/,
+                loader: "url-loader"
+            },
+            {
+                test: /\.(scss|css)/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'template.html')
+        })
+    ]
 }
